b4ed9b4d80509fcf480e4ca98281a1f10d9d9317
saitej34 <saiteja6734.yelagandula@gmail.com>
2023-08-22 00:44:07 +0530

    typecasting

---
+++ OOPS/typecasting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc  = new Scanner(System.in); 
+        System.out.println((int)10.23/2.0);            // output :  5.0 
+        System.out.println((int)10.23/2);            // output :  5
+        System.out.println((float)10/5.0);             // output : 2.0 
+        System.out.println((double)10/5.0);          // output : 2.0
+        System.out.println((int)10/5.0);            // output 2.0
+    }
+}
