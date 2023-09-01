0f7a0ba1faff183f2cf8066c63979f5cb27b4ab3
saitej34 <saiteja6734.yelagandula@gmail.com>
2023-09-01 22:47:33 +0530

    valid

---
+++ Stacks/Longestvalidparenthesis.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public int longestValidParentheses(String s) 
+    {
+        Stack<Integer> st = new Stack<>();
+        int max = 0;
+        st.push(-1);
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if(st.isEmpty()==false)
+                {
+                    max = Math.max(max,i-st.peek());
+                }
+                else
+                {
+                    st.push(i);
+                }
+            }
+        }
+        return max;
+    }
+}
